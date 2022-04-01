afd4b7bbb10af98aca604a8ea77b6557ecebf714
PieroMarraffa <pieromarraffa.business@gmail.com>
2022-04-01 15:19:14 +0200

    overloadOperator

---
+++ OverloadOperatori/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OverloadOperatori;
+Console.WriteLine("BENVENUTO!!!");
+Console.WriteLine("");
+Console.WriteLine("INSERISCI IL LATO DEL PRIMO QUADRATO:");
+
+Quadrato quadrato1 = new Quadrato();
+bool avanti = false;
+do
+{
+    try
+    {
+        double lato1 = double.Parse(Console.ReadLine());
+        quadrato1 = new Quadrato(lato1);
+        avanti = true;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("INSERISCI UN NUMERO VALIDO: ");
+    }
+} while (!avanti);
+
+
+Console.WriteLine("");
+Console.WriteLine("INSERISCI IL LATO DEL SECONDO QUADRATO:");
+Quadrato quadrato2 = new Quadrato();
+bool avanti2 = false;
+do
+{
+    try
+    {
+        double lato2 = double.Parse(Console.ReadLine());
+        quadrato2 = new Quadrato(lato2);
+        avanti2 = true;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("INSERISCI UN NUMERO VALIDO: ");
+    }
+} while (!avanti2);
+
+Console.WriteLine("");
+Console.WriteLine("LA SOMMA DEI DUE QUADRATI RESTITUISCE UN QUADRATO DI LATO: ");
+Console.WriteLine((quadrato1 + quadrato2).getLato());
---
+++ OverloadOperatori/Quadrato.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperatori
+{
+    internal class Quadrato
+    {
+        private double lato;
+
+        public Quadrato() { }
+
+        public Quadrato(double l)
+        {
+            lato = l;
+        }
+
+        public double getLato()
+        {
+            return lato;
+        }
+
+        public void setLato(double l)
+        {
+            this.lato = l;
+        }
+
+        public double getPerimetro()
+        {
+            return (lato * 4);
+        }
+
+        public double getArea()
+        {
+            return (lato * lato);
+        }
+
+        public static Quadrato operator +(Quadrato q1, Quadrato q2)
+        {
+            return (new Quadrato(Math.Sqrt(q1.getArea() + q2.getArea())));
+        }
+
+    }
+}
