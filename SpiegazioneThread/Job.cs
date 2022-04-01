5a6b702051e061fd1e8c1f7ecf857eca1a9b51c8
PieroMarraffa <pieromarraffa.business@gmail.com>
2022-04-01 16:18:04 +0200

    spiegazione thread e call back

---
+++ SpiegazioneThread/Job.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiegazioneThread
+{
+
+    public delegate void JobMessage(string message);
+    internal class Job
+    {
+        private string nome;
+        private int delay;
+
+        public event JobMessage Invia;
+
+        public Job(string Nome, int Delay)
+        {
+            nome = Nome;
+            delay = Delay;
+        }
+
+        public void DoWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Invia(this.nome + ": " + this.delay);
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
---
+++ SpiegazioneThread/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpiegazioneThread;
+
+//Job job = new Job("Job 1", 500);
+//Job job2 = new Job("Job 2", 500);
+
+//Console.WriteLine("Start: " + DateTime.Now.ToLongTimeString());
+//job.DoWork();
+//job2.DoWork();
+//Console.WriteLine("Finish: " + DateTime.Now.ToLongTimeString());
+
+
+//così dovrebbero partire in parallelo
+//Console.WriteLine("Start: " + DateTime.Now.ToLongTimeString());
+//Thread th1 = new Thread(job.DoWork);
+//Thread th2 = new Thread(job2.DoWork);
+//th1.Start();
+//th2.Start();
+//th1.Join();
+//th2.Join();
+//Console.WriteLine("Finish: " + DateTime.Now.ToLongTimeString());
+
+//stampa start e finish perché girano sul main mentre le altre girano
+//in parallelo sui thread
+
+//con il JOIN dico al main di attendere la fine dei due thread
+
+//Job job3 = new Job("Job 3", 500);
+//Console.WriteLine("Start: " + DateTime.Now.ToLongTimeString());
+//Thread th1 = new Thread(job.DoWork);
+//Thread th2 = new Thread(job2.DoWork);
+//Thread th3 = new Thread(job3.DoWork);
+//th1.Start();
+//th2.Start();
+//th3.Start();
+//th1.Join();
+//th2.Join();
+//th3.Join();
+//Console.WriteLine("Finish: " + DateTime.Now.ToLongTimeString());
+
+//ora ho visto che funziona veramente in parallelo
+
+internal class Program
+{
+
+    public static void Main(string [] args)
+    {
+
+        Job job1 = new Job("Job 1", 500);
+        Job job2 = new Job("Job 2", 500);
+        Job job3 = new Job("Job 3", 500);
+
+        job1.Invia += EventCallback;
+        job2.Invia += EventCallback;
+        job3.Invia += EventCallback;
+
+        Thread th1 = new Thread(job1.DoWork);
+        Thread th2 = new Thread(job2.DoWork);
+        Thread th3 = new Thread(job3.DoWork);
+        th1.Start();
+        th2.Start();
+        th3.Start();
+        th1.Join();
+        th2.Join();
+        th3.Join();
+    }
+
+    private static void EventCallback(string message)
+    {
+        Console.WriteLine("Main: " + message);
+    }
+}
